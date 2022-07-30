a44a304b9afdf7578728fab7d6fbb508f4a234a3
appkumark <appk.kanubuddi@gmail.com>
2022-07-31 00:03:35 +0530

    recursiveFactorial, iterativeFactorial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(iterativeFactorial(3));
+        System.out.println(recursiveFactorial(4));
+    }
+
+    public static int recursiveFactorial(int num){
+
+        if(num == 0){
+            return 1;
+        }
+
+        return num * recursiveFactorial(num - 1);
+    }
+
+    public static int iterativeFactorial(int num){
+
+        if(num == 0) {
+            return 1;
+        }
+
+        int factorial = 1;
+        for ( int i = 1; i <= num; i++){
+            factorial *= i;
+        }
+
+        return factorial;
     }
 }
